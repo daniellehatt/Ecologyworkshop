6be077d4dbe840a969a179942a7bc96aee246fe3
Danielle Hatt <daniellehatt@Danielles-MBP.attlocal.net>
2020-03-30 19:19:52 -0400

    Update 3/30

--- Final Project Analysis.R
+++ Final Project Analysis.R
@@ -33,8 +33,12 @@ axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 adf.test(SB1) #ts is not stationary:Reject the null hyp
 adf.test(diff(SB1,1))
 acf(diff(SB1, 1), lag.max=4)
+pacf(diff(SB1, 1), lag.max=4)
 arima.SB2 <-auto.arima((SB1), trace=TRUE)
 SB2= diff(SB1, differences = 2, lag = 4)
+adf.test(SB2)
+acf((SB2), lag.max=4)
+pacf((SB2), lag.max=4)
 plot(SB2)
 arima.SB2 <-auto.arima((SB2), trace=TRUE)
 tsdisplay(residuals(arima.SB2), lag.max=4)
@@ -43,8 +47,17 @@ par(mfrow=c(1,1), mai=c(1,1,1,1))
 plot(SB2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none", main="ARIMA model fitted to biomass data for Sprigger Bank"); lines(fitted(arima.SB2),col="red")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 checkresiduals(arima.SB2, lag=4)
-plot(forecast(arima.SB2, h=15), xaxt="none", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time")
-axis(1,at=seq(1,15,1),  labels=seq(2007,2021,1))
+plot(forecast(arima.SB2, h=20), xaxt="none", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time")
+axis(1,at=seq(1,21,1),  labels=seq(2007,2027,1))
+summary(Tab.SB$Temperature)
+summary(Tab.DK$Temperature)
+summary(Tab.BA$Temperatire)
+mean(Tab.SB$Temperatire)
+sd(Tab.SB$Temperature)
+summary(Tab.SB$Biomass)
+summary(Tab.DK$Biomass)
+summary(Tab.BA$Biomass)
+
 
 #Explanatory series for Sprigger Bank
 #Explanatory series: Temperature
@@ -58,27 +71,27 @@ temp <- tsclean(temp)
 temp.d <- decompose(temp, 'multiplicative') 
 plot(temp.d)
 adf.test(temp)
-temp2= diff(temp, differences = 1, lag = 4)
-temp2
-SB2
-plot(temp2)
-ccf(c(temp2),SB2, na.action = na.pass, lag.max=10, plot=TRUE)
-arima.SB3 <-auto.arima(SB2, xreg=c(temp2), trace=TRUE)
+temp2.SB= diff(temp, differences = 2, lag = 4)
+temp2.SB
+adf.test(temp2.SB)
+acf(temp2.SB)
+pacf(temp2.SB)
+plot(temp2.SB)
+ccf(temp2.SB,SB2, na.action = na.pass, lag.max=4, plot=TRUE)
+arima.SB3 <-auto.arima(SB2, xreg=c(temp2.SB), trace=TRUE)
 AIC(arima.SB2, arima.SB3)
-
-?auto.arima()
-
-
 plot(SB2 , typ="l", ylab="Biomass", xlab="Time"); lines(fitted(arima.SB3),col="red")
 temp.i <- temp
-temp.i[temp.i < 28 ]<- 0 
-temp.i[temp.i >= 28 ]<- 1
+temp.i[temp.i < 29 ]<- 0 
+temp.i[temp.i >= 29 ]<- 1
 plot(temp.i, ylab="Temperature", xlab="Time")
-arima.SB4 <-auto.arima(SB1, xreg=temp.i, trace=TRUE)
+temp.i=diff(temp.i, differences= 2, lag =4)
+arima.SB4 <-auto.arima(SB2, xreg=temp.i, trace=TRUE)
 AIC(arima.SB2,arima.SB4 )
-checkresiduals(arima.SB4, lag=36)
+AIC(arima.SB3,arima.SB4 )
+checkresiduals(arima.SB3, lag=36)
 par(mfrow=c(1,1))
-plot(SB1 , typ="l", ylab="Biomass", xlab="Time"); lines(fitted(arima.SB4),col="red")
+plot(SB2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none"); lines(fitted(arima.SB3),col="red")
 
 #Explanatory series for salinity
 Tab.SB
@@ -90,36 +103,40 @@ sal <- tsclean(sal)
 sal.d <- decompose(sal, 'multiplicative') 
 plot(sal.d)
 adf.test(sal)
-adf.test(diff(sal))
-sal2= diff(sal, differences = 1, lag = 4)
+sal2= diff(sal, differences = 2, lag = 4)
 sal2
+adf.test(sal2)
+acf(sal2)
+pacf(sal2)
 plot(sal2)
-ccf(c(sal2),SB2, na.action = na.pass, lag.max=4, plot=TRUE)
-arima.SB5 <-auto.arima(SB1, xreg=c(diff(sal),0), trace=TRUE)
+ccf(sal2,SB2, na.action = na.pass, lag.max=4, plot=TRUE)
+plot(sal2)
+arima.SB5 <-auto.arima(SB2, xreg=sal2, trace=TRUE)
 AIC(arima.SB2, arima.SB5 )
 sal.i <- sal 
 sal.i[sal.i < 24 ]<- 0 
 sal.i[sal.i >= 24 ]<- 1
 plot(sal.i)
-arima.SB6 <-auto.arima(SB1, xreg=sal.i, trace=TRUE)
+sal.i=diff(sal.i, differences= 2, lag =4)
+arima.SB6 <-auto.arima(SB2, xreg=sal.i, trace=TRUE)
 AIC(arima.SB2,arima.SB6 )
-checkresiduals(arima.SB6, lag=36)
+checkresiduals(arima.SB6, lag=4)
 par(mfrow=c(1,1), mai=c(0.5,0.5,0.5, 0.5))
-plot(SB1 , typ="l", ylab="", xlab="", xaxt="none", main=c("Figure 5 showing ARIMA model with extreme salinity", "as a driver of biomass at Duck Key")); lines(fitted(arima.SB5),col="red")
+plot(SB1 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with extreme salinity", "as a driver of biomass at Sprigger Bank")); lines(fitted(arima.SB5),col="red")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 
 #explanatory variable: salinity and temperature
-x=c(temp,sal)
+x=c(temp2.SB,sal2)
 x
-y=cbind(temp,sal)
+y=cbind(temp2,sal2)
 adf.test(x)
-adf.test(diff(x))
-arima.SB7 <-auto.arima(SB1, xreg=y, trace=TRUE)
-AIC(arima.SB2,arima.SB7 )
-par(mfrow=c(1,0), mai=c(0.5,0.5,0.5, 0.5))
-plot(SB1 , typ="l", ylab="", xlab="", xaxt="none", main=c("Figure 6 showing ARIMA model with temperature", "and salinity as a drivers of biomass at Duck Key"));
+arima.SB7 <-auto.arima(SB2, xreg=y, trace=TRUE)
+AIC(arima.SB2,arima.SB7)
+AIC(arima.SB3,arima.SB7)
+par(mfrow=c(1,1), mai=c(1,1,1,1))
+plot(SB2 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with temperature", "and salinity as a drivers of biomass at Sprigger Bank"));
 lines(fitted(arima.SB7),col="red")
-axis(1,at=seq(1,10,1),  labels=seq(2007,2016,1))
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 
 z=c(temp.i,sal.i)
 z
@@ -162,6 +179,89 @@ checkresiduals(arima.DK1, lag=4)
 plot(forecast(arima.DK1, h=20), xaxt="none",  ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time")
 axis(1,at=seq(1,15,1),  labels=seq(2007,2021,1))
 
+#Explanatory series for Duck Key
+#Explanatory series: Temperature
+Tab.DK
+Tab.DK$Temperature
+temp <- ts(Tab.DK$Temperature, start= 1, frequency=4)
+par(mfrow=c(1,1), mai=c(0.9,0.9,0.9, 0.9)) 
+plot(temp, typ="l", ylab= "Temperature", xlab="Time")
+lines(tsclean(temp) , col="red")
+temp <- tsclean(temp)
+temp.d <- decompose(temp, 'multiplicative') 
+plot(temp.d)
+adf.test(temp)
+temp2.DK= diff(temp, differences = 2, lag = 4)
+temp2.DK
+adf.test(temp2.DK)
+acf(temp2.DK)
+pacf(temp2.DK)
+plot(temp2.DK)
+ccf(temp2.DK,DK2, na.action = na.pass, lag.max=4, plot=TRUE)
+arima.DK3 <-auto.arima(DK2, xreg=c(temp2.DK), trace=TRUE)
+AIC(arima.DK1, arima.DK3)
+plot(DK2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none"); lines(fitted(arima.DK3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+temp.i <- temp2
+temp.i[temp.i < 29 ]<- 0 
+temp.i[temp.i >= 29 ]<- 1
+plot(temp.i, ylab="Temperature", xlab="Time")
+arima.DK4 <-auto.arima(DK2, xreg=temp.i, trace=TRUE)
+AIC(arima.DK1,arima.DK4 )
+AIC(arima.DK3,arima.DK4 )
+checkresiduals(arima.DK3, lag=4)
+par(mfrow=c(1,1))
+plot(DK2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none"); lines(fitted(arima.DK3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
+#Explanatory series for salinity
+Tab.DK
+sal <- ts(Tab.DK$Salinity, start= 1, frequency=4)
+sal
+par(mfrow=c(1,1), mai=c(0.25,0.8,0.1, 0.1)) 
+plot(sal, typ="l", ylab= "Salinity", xlab="Time")
+lines(tsclean(sal) , col="red")
+sal <- tsclean(sal)
+sal.d <- decompose(sal, 'multiplicative') 
+plot(sal.d)
+adf.test(sal)
+sal2= diff(sal, differences = 2, lag = 4)
+sal2
+adf.test(sal2)
+acf(sal2)
+pacf(sal2)
+plot(sal2)
+ccf(sal2,DK2, na.action = na.pass, lag.max=4, plot=TRUE)
+plot(sal2)
+arima.DK5 <-auto.arima(DK2, xreg=sal2, trace=TRUE)
+AIC(arima.DK1, arima.DK5 )
+sal.i <- sal 
+sal.i[sal.i < 35 ]<- 0 
+sal.i[sal.i >= 35 ]<- 1
+plot(sal.i)
+sal.i= diff(sal, differences = 2, lag = 4)
+arima.DK6 <-auto.arima(DK2, xreg=sal.i, trace=TRUE)
+AIC(arima.DK2,arima.DK6 )
+AIC(arima.DK2,arima.DK6 )
+checkresiduals(arima.DK6, lag=4)
+par(mfrow=c(1,1), mai=c(0.5,0.5,0.5, 0.5))
+plot(DK1 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with extreme salinity", "as a driver of biomass at Sprigger Bank")); lines(fitted(arima.DK5),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
+#explanatory variable: salinity and temperature
+x=c(temp2,sal2)
+x
+y=cbind(temp2,sal2)
+adf.test(x)
+arima.DK7 <-auto.arima(DK2, xreg=y, trace=TRUE)
+AIC(arima.DK1,arima.DK7)
+AIC(arima.DK3,arima.DK7)
+par(mfrow=c(1,1), mai=c(1,1,1,1))
+plot(DK2 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with temperature", "and salinity as a drivers of biomass at Sprigger Bank"));
+lines(fitted(arima.DK7),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
+
 #Bob Allen: Time Series
 file.choose()
 Tab.BA=read.csv("/Users/daniellehatt/Desktop/BSC 6926/Final Project Datasets/LTER.BA.csv", header = TRUE)
@@ -195,6 +295,88 @@ checkresiduals(arima.BA2, lag=4)
 plot(forecast(arima.BA2, h=20), xaxt="none")
 axis(1,at=seq(1,15,1),  labels=seq(2007,2021,1))
 
+#Explanatory series for Bob Allen
+#Explanatory series: Temperature
+Tab.BA
+Tab.BA$Temperatire
+temp <- ts(Tab.BA$Temperatire, start= 1, frequency=4)
+par(mfrow=c(1,1), mai=c(0.9,0.9,0.9, 0.9)) 
+plot(temp, typ="l", ylab= "Temperature", xlab="Time")
+lines(tsclean(temp) , col="red")
+temp <- tsclean(temp)
+temp.d <- decompose(temp, 'multiplicative') 
+plot(temp.d)
+adf.test(temp)
+temp2= diff(temp, differences = 1, lag = 4)
+temp2
+adf.test(temp2)
+acf(temp2)
+pacf(temp2)
+plot(temp2)
+ccf(temp2,BA2, na.action = na.pass, lag.max=4, plot=TRUE)
+arima.BA3 <-auto.arima(BA2, xreg=c(temp2), trace=TRUE)
+AIC(arima.BA2, arima.BA3)
+plot(BA2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none"); lines(fitted(arima.BA3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+temp.i <- temp2
+temp.i[temp.i < 29 ]<- 0 
+temp.i[temp.i >= 29 ]<- 1
+plot(temp.i, ylab="Temperature", xlab="Time")
+arima.BA4 <-auto.arima(BA2, xreg=temp.i, trace=TRUE)
+AIC(arima.BA2,arima.BA4 )
+AIC(arima.BA3,arima.BA4 )
+checkresiduals(arima.BA3, lag=4)
+par(mfrow=c(1,1))
+plot(BA2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none"); lines(fitted(arima.BA3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
+#Explanatory series for salinity
+Tab.BA
+sal <- ts(Tab.BA$Salinity, start= 1, frequency=4)
+sal
+par(mfrow=c(1,1), mai=c(0.25,0.8,0.1, 0.1)) 
+plot(sal, typ="l", ylab= "Salinity", xlab="Time")
+lines(tsclean(sal) , col="red")
+sal <- tsclean(sal)
+sal.d <- decompose(sal, 'multiplicative') 
+plot(sal.d)
+adf.test(sal)
+sal2= diff(sal, differences = 1, lag = 4)
+sal2
+adf.test(sal2)
+acf(sal2)
+pacf(sal2)
+plot(sal2)
+ccf(sal2,BA2, na.action = na.pass, lag.max=4, plot=TRUE)
+plot(sal2)
+arima.BA5 <-auto.arima(BA2, xreg=sal2, trace=TRUE)
+AIC(arima.BA2, arima.BA5 )
+sal.i <- sal 
+sal.i[sal.i < 35 ]<- 0 
+sal.i[sal.i >= 35 ]<- 1
+plot(sal.i)
+sal.i= diff(sal, differences = 1, lag = 4)
+arima.BA6 <-auto.arima(BA2, xreg=sal.i, trace=TRUE)
+AIC(arima.BA2,arima.BA6 )
+AIC(arima.BA5,arima.BA6 )
+checkresiduals(arima.BA6, lag=4)
+par(mfrow=c(1,1), mai=c(0.5,0.5,0.5, 0.5))
+plot(BA1 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with extreme salinity", "as a driver of biomass at Sprigger Bank")); lines(fitted(arima.BA5),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
+#explanatory variable: salinity and temperature
+x=c(temp2,sal2)
+x
+y=cbind(temp2,sal2)
+adf.test(x)
+arima.BA7 <-auto.arima(BA2, xreg=y, trace=TRUE)
+AIC(arima.BA2,arima.BA7)
+AIC(arima.BA3,arima.BA7)
+par(mfrow=c(1,1), mai=c(1,1,1,1))
+plot(BA2 , typ="l", ylab="", xlab="", xaxt="none", main=c("ARIMA model with temperature", "and salinity as a drivers of biomass at Sprigger Bank"));
+lines(fitted(arima.BA7),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+
 
 #Biomass for each site
 dev.off()
@@ -208,8 +390,9 @@ axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 
 #Decomposition for each site
 par(mfrow=c(1,3))
-plot(SB.d1, xaxt="none", xlab="")
+p1=plot(SB.d1, xaxt="none", xlab="")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+p1
 plot(BA.d1, xaxt="none", xlab="")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 plot(DK.d1, xaxt="none", xlab="")
@@ -219,18 +402,25 @@ axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 par(mfrow=c(1,3))
 plot(SB2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="", xaxt="none", main="ARIMA model fitted to biomass data for Sprigger Bank"); lines(fitted(arima.SB2),col="red")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
-plot(BA2 , typ="l", ylab= "", xlab="Time", xaxt="none", main="ARIMA model fitted to Biomass data for Penicillus"); lines(fitted(arima.BA2),col="red")
+plot(BA2 , typ="l", ylab= "", xlab="Time", xaxt="none", main="ARIMA model fitted to biomass data for Bob Allen Keys"); lines(fitted(arima.BA2),col="red")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 plot(DK2 , typ="l", ylab= "", xlab="", xaxt="none", main="ARIMA model fitted to biomass data for Duck Key"); lines(fitted(arima.DK1),col="red")
 axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
 
 #Forecasting for each site
 par(mfrow=c(1,3))
-plot(forecast(arima.SB2, h=20), xaxt="none", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="")
+plot(forecast(arima.SB3, xreg=temp2.SB, h=5), xaxt="none", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="")
 axis(1,at=seq(1,25,1),  labels=seq(2007,2031,1))
-plot(forecast(arima.DK1, h=20), xaxt="none",  ylab= "", xlab="Time")
+plot(forecast(arima.BA2, h=36), xaxt="none", ylab= "", xlab="Time")
 axis(1,at=seq(1,25,1),  labels=seq(2007,2031,1))
-plot(forecast(arima.BA2, h=20), xaxt="none", ylab= "", xlab="")
+plot(forecast(arima.DK3, xreg=temp2.DK, h=20), xaxt="none",  ylab= "", xlab="")
 axis(1,at=seq(1,25,1),  labels=seq(2007,2031,1))
 
-
+#Explanatory series for each site
+par(mfrow=c(1,2))
+AIC(arima.SB2, arima.SB3)
+plot(SB2, typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none", main=c("ARIMA model with temperature", "as a driver of biomass at Sprigger Bank")); lines(fitted(arima.SB3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
+AIC(arima.DK1, arima.DK3)
+plot(DK2 , typ="l", ylab= "Biomass"~"("~"g/m"^{2}~"per quadrat"~")", xlab="Time", xaxt="none",  main=c("ARIMA model with temperature", "as a driver of biomass at Duck Key")); lines(fitted(arima.DK3),col="red")
+axis(1,at=seq(1,12,1),  labels=seq(2007,2018,1))
